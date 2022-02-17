bd7580ee7d59984f09430fad1c42b4887630f4dd
QuentinP <puechguiral81@hotmail.fr>
2022-02-17 02:50:29 +0100

    PoC which works -v1.1

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # notionput
 Various scripts to edit a Notion based on external sources
+
+
+## Progress of the script :
+
+- [x] Retrieve tweets of a user with his id
+- [x] Saves tweets in columns on the dashboard notion
+- [ ] Automatically retrieve the user's name from the notion dashboard
+- [ ] Reverse the order of tweets on the notion dashboard (Newest first)
+- [ ] Other suggestions
+
+## How to use this script : 
+
+Create a developer twitter account and get this "BEARER_TOKEN".
+Add this token as environment variable "NOTION_BEARER_TOKEN"
+Create an integration in notion and get the token, don't forget to share the document with the integration.
+Add this token as environment variable "NOTION_TOKEN_PUT"
+
+Add the id of the notion page in the main_page_id variable
+
+Finally, write in the "Usernames" table the @ of the users whose tweets you want to retrieve
+
+### Exemple after execution : 
+
+![Notion page with some tweets](Exemple.png)
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import requests
+import tweepy
 import json
 import os
 
 token = os.environ["NOTION_TOKEN_PUT"]
+BEARER_TOKEN = os.environ["NOTION_BEARER_TOKEN"]
 
-block_id = 'e42cdf2c-ef06-4059-b88c-2b159415c902'
-block_page_id = 'fa2142e7178a4af980ef0bc2bcde14ec'
-
-url_block_page = f"https://api.notion.com/v1/blocks/{block_page_id}/children"
-url = f"https://api.notion.com/v1/blocks/{block_id}"
+main_page_id = 'fa2142e7178a4af980ef0bc2bcde14ec'
 
 headers = {
     "Accept": "application/json",
@@ -17,13 +15,23 @@ headers = {
     "Authorization": "Bearer " + token
 }
 
+Usernames = ['NotionAPI','berty','DFintelligence','moul']
+
 
 def main():
 
-    #print(updateEmbed())
+    tweet_ids_by_user = []
+
+    ColumnIDs = getColumnIDs()
+
+    for username in Usernames:
+        tweet_ids_by_user.append(getTweet(username))
     
-    print(appendEmbed())
-    
+    for index,tweet_ids_list in enumerate(tweet_ids_by_user):
+        for tweet in tweet_ids_list:
+            appendEmbed(tweet,Usernames[index],ColumnIDs[index])
+            
+"""
 # Appel de l'ensemble des blocks de la page via l'id de page    
 
 def apiCall():
@@ -31,9 +39,9 @@ def apiCall():
     response = requests.request("GET", url_block_page, headers=headers)
 
     return json.loads(response.text)
-
+"""
 #Met à jour le lien du tweet dans le block "embed" via son id (block_id)
-
+'''
 def updateEmbed():
     
     updateData = {
@@ -43,22 +51,61 @@ def updateEmbed():
     }
 
     return requests.request("PATCH", url, json=updateData, headers=headers)
-
+'''
 #Créer un nouveau block "embed" qui contient le lien vers le tweet
 
-def appendEmbed():
+def appendEmbed(tweet_ids,username,column_id):
+
+    url_block_column = f"https://api.notion.com/v1/blocks/{column_id}/children"
 
     appData = [{
             "object": "block",
             "type": "embed",
             "embed": {
-                "url": "https://twitter.com/ZeratoR/status/1493276555052826624?s=20&t=UFnSjZGiFRcZQ9lHUv0JtA"
+                "url": f"https://twitter.com/{username}/status/{tweet_ids}"
             }
         }
     ]
-    
-    return requests.request("PATCH", url_block_page, json={"children": appData}, headers=headers)
 
+    ReturnCode = requests.request("PATCH", url_block_column, json={"children": appData}, headers=headers)
+        
+    return ReturnCode
+
+
+def getColumnIDs():
+
+    column_id_return = []
+        #On récupére l'id du block "column_list" qui est un enfant de l'id de la page
+    url_block_page_main = f"https://api.notion.com/v1/blocks/{main_page_id}/children"
+
+    block_column_list_id = json.loads(requests.request("GET", url_block_page_main, headers=headers).text)
+    block_column_list_id = block_column_list_id["results"][0]["id"]
+        #Puis on récupére l'id des x colonnes de la page
+    url_block_page_column = f"https://api.notion.com/v1/blocks/{block_column_list_id}/children"
+
+    columns_id = json.loads(requests.request("GET", url_block_page_column, headers=headers).text)
+
+    for column_id in columns_id["results"]:
+        column_id_return.append(column_id["id"])
+
+    return column_id_return
+
+
+def getTweet(username):
+
+    ids = []
+
+    client = tweepy.Client(bearer_token=BEARER_TOKEN)
+
+        #On récupére l'id Twitter de l'utilisateur
+    UserID = (client.get_user(username=username)).data.id
+
+        #On affiche ses 5 derniers tweets
+    tweets = client.get_users_tweets(id=UserID, max_results=5)
+    for tweet in tweets.data:
+        ids.append(tweet.id)
+
+    return ids
 
 if __name__ == '__main__':
     main()
